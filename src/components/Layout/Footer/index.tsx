c5fbd197ff12ff22bd3faa923cc287bcc3619db9
Andy Camann <acamann@gmail.com>
2020-11-25 15:38:30 -0600

    add footer style and check puzzle button

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <h3>Footer</h3>
-    </footer>
+    <View style={styles.footer}>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    backgroundColor: "#008877",
+    boxShadow: "0 1px 0 rgba(0,0,0,.2)",
+    height: 50,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    lineHeight: 50,
+    paddingLeft: 20,
+    textTransform: "capitalize",
+  }
+})
 
 export default Footer;
--- src/root/Home/index.tsx
+++ src/root/Home/index.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+
 import { Button, ScrollView, StyleSheet, View } from "react-native";
-import Header from "../../components/Layout/Header";
-import Sudoku from "../../components/Sudoku";
-import { getPuzzle } from "../../store/sudoku";
+
 import { Store } from "../../store";
 import { Puzzle } from "../../store/types";
+import { getPuzzle } from "../../store/sudoku";
+
+import Header from "../../components/Layout/Header";
+import Sudoku from "../../components/Sudoku";
+import Footer from "../../components/Layout/Footer";
 
 const Home: React.FC = () => {
   const dispatch = useDispatch();
   const puzzle: Puzzle = useSelector((store: Store) => store.sudoku.puzzle);
 
-  const handlePress = () => {
-    dispatch(getPuzzle());
-  }
+  const handleGetPuzzle = (): void => { dispatch(getPuzzle()); }
+  const handleCheckPuzzle = (): void => { return; }
 
   return (
     <ScrollView
-      style={styles.container}
+      contentContainerStyle={styles.container}
     >
       <Header />
       <View style={styles.main}>
-        <Button title="Get Puzzle" onPress={handlePress} />
+        <Button title="Get Puzzle" onPress={handleGetPuzzle} />
         <Sudoku puzzle={puzzle} />
+        <Button title="Check Puzzle" onPress={handleCheckPuzzle} />
       </View>
+      <Footer />
     </ScrollView>
   );
 }
@@ -31,12 +36,10 @@ const Home: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "whitesmoke",
-  },
-  centering: {
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-    height: '100vh',
+    height: "100vh",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "column"
   },
   main: {
     padding: 16,
