50c689d6c5050294197f909e4ff84603ffaf1ae1
Andy Camann <acamann@gmail.com>
2020-12-14 21:22:51 -0600

    clean

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,20 +1,13 @@
-// MODELS
+// Suoku MODELS
 export type Puzzle = {
   board: number[][];
   solution: number[][];
 }
 
-export const FETCH_PUZZLE = 'sudoku/fetchPuzzle'
-export const PUZZLE_FETCHED = 'sudoku/puzzleFetched'
+export const FETCH_PUZZLE = 'sudoku/fetchPuzzle';
+export const PUZZLE_FETCHED = 'sudoku/puzzleFetched';
 
-interface FetchSudokuAction {
-  type: typeof FETCH_PUZZLE;
-  difficulty: "easy" | "medium" | "hard";
-}
+interface FetchSudokuAction { type: typeof FETCH_PUZZLE; difficulty: "easy" | "medium" | "hard"; }
+interface SudokuFetchedAction { type: typeof PUZZLE_FETCHED; puzzle: Puzzle; }
 
-interface SudokuFetchedAction {
-  type: typeof PUZZLE_FETCHED;
-  puzzle: Puzzle;
-}
-
-export type SudokuActionTypes = FetchSudokuAction | SudokuFetchedAction
+export type SudokuActionTypes = FetchSudokuAction | SudokuFetchedAction;
